00ca7d44a4afd2740bf7f4728390818530ced14d
yoonPuddin <minijeff@hansung.ac.kr>
2019-11-30 16:25:44 +0900

    Second Commit

--- Hello1.java
+++ Hello1.java
@@ -1,6 +1,8 @@
 public class Hello1{
 	public static void main(String[] args){
 		System.out.println("hello, world from GIT");
-	}
+		System.out.println("Changes after First commit!");
+	}               
 }
+
 
